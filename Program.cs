0d88d62a963d7e502a73a5d720fe487af5a3be1e
freddo2300 <fsf96@hotmail.com>
2023-09-27 15:35:49 +0200

    resolve conflicts

--- Program.cs
+++ Program.cs
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-=======
 using Microsoft.EntityFrameworkCore;
-
-using WebAPI.Models;
 
 namespace WebAPI
 {
@@ -19,45 +7,26 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            // Add services to the container.
 
             builder.Services.AddControllers();
-
-            // Adding the database context with connection string.
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
->>>>>>> main
 
-var app = builder.Build();
+            var app = builder.Build();
 
-<<<<<<< HEAD
-// Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
-=======
             // Configure the HTTP request pipeline.
-
-            app.UseSwagger();
-            app.UseSwaggerUI();
+                app.UseSwagger();
+                app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
->>>>>>> main
 
-app.UseHttpsRedirection();
+            app.UseAuthorization();
 
-app.UseAuthorization();
+            app.MapControllers();
 
-<<<<<<< HEAD
-app.MapControllers();
-
-app.Run();
-=======
             app.Run();
         }
     }
 }
->>>>>>> main
