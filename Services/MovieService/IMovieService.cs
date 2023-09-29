c506de81fa49fe0ae9867d49aa56b8672b5b556e
mansourhamidi94 <MansourHamidi@hotmail.dk>
2023-09-29 16:22:17 +0200

    Services

--- Services/MovieService/IMovieService.cs
+++ Services/MovieService/IMovieService.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 using WebAPI.Data.Entities;
 
 namespace WebAPI.Services.MovieService
@@ -5,17 +7,23 @@ namespace WebAPI.Services.MovieService
     public interface IMovieService
     {
         // Create
-        Task CreateMovie(Movie movie);
+        Task<Movie> CreateMovie(Movie movie);
+
+        Task<Movie> PostMovie(Movie movie);
+
+        Task<IEnumerable<Movie>> GetMovies();
+
+        Task<Movie> PutMovie(int id, Movie movie);
 
         // Read
-        Task<ICollection<Movie>> GetMovie();
+        // Task<ICollection<Movie>> GetMovie();
+        Task<Movie> GetMovie(int id);
 
-        Task<Movie> GetMovieById(int id);
+        Task<Movie> DeleteMovie(int id);
 
         // Update
         Task UpdateMovie(Movie movie);
 
         // Delete
-        Task DeleteMovie(int id);
     }
 }
